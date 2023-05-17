92b9d6153283d97820cb5b0f025d7a1b07c71c06
Nexoria Studios <nexoriastudios@gmail.com>
2023-05-16 23:26:00 -0300

    v1.6
    - Primeira versão perfeitamente jogável
    - Feito o menu de análise para a hidroelétrica
    - Configurado botão "Ctrl Esquerdo" para voltar ao menu

--- Projeto Escolar - Fontes de Energia/Assets/Scripts/C#/Menu/MenuManager.cs
+++ Projeto Escolar - Fontes de Energia/Assets/Scripts/C#/Menu/MenuManager.cs
@@ -8,24 +8,109 @@ public class MenuManager : MonoBehaviour {
 [SerializeField] private GameObject menuOpcoes;
 [SerializeField] private GameObject cameraOpcoes;
 [SerializeField] private GameObject cameraMenu;
+[SerializeField] private GameObject menuAnalisar;
+[SerializeField] private GameObject cameraAnalisar;
+[SerializeField] private GameObject cameraUIAnalisar;
+[SerializeField] private GameObject painelAnaliseHidroeletrica;
+[SerializeField] private GameObject painelAnaliseEolica;
+[SerializeField] private GameObject painelAnaliseSolar;
+[SerializeField] private GameObject cameraSolar;
+[SerializeField] private GameObject cameraEolica;
+[SerializeField] private GameObject cameraHidroeletrica;
 [SerializeField] private GameObject posProcessamentoJogo;
 [SerializeField] private GameObject posProcessamentoMenu;
 [SerializeField] private GameObject posProcessamentoOpcoes;
+
+private void Update()
+{
+    bool apertouCTRL = Input.GetButton("Fire1");
+
+    if(apertouCTRL && jogador.activeSelf)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        retornarAoMenu();
+    }
+    else
+    {
+
+    }
+}
 
 void Start()
 {
     menuInicial.SetActive(true);
     menuOpcoes.SetActive(false);
     cameraMenu.SetActive(true);
+    menuAnalisar.SetActive(false);
+    cameraUIAnalisar.SetActive(false);
     cameraOpcoes.SetActive(false);
+    cameraAnalisar.SetActive(false);
+    cameraEolica.SetActive(false);
+    cameraSolar.SetActive(false);
+    cameraHidroeletrica.SetActive(false);
+    painelAnaliseEolica.SetActive(false);
+    painelAnaliseHidroeletrica.SetActive(false);
+    painelAnaliseSolar.SetActive(false);
     jogador.SetActive(false);
+}
+
+public void analisarHidroeletrica()
+{
+    cameraUIAnalisar.SetActive(false);
+    cameraAnalisar.SetActive(false);
+    menuAnalisar.SetActive(false);
+    cameraHidroeletrica.SetActive(true);
+    painelAnaliseHidroeletrica.SetActive(true);
+}
+
+public void analisarSolar()
+{
+    cameraUIAnalisar.SetActive(false);
+    cameraAnalisar.SetActive(false);
+    menuAnalisar.SetActive(false);
+    cameraSolar.SetActive(true);
+    painelAnaliseSolar.SetActive(true);
+}
+
+public void analisarEolica()
+{
+    cameraUIAnalisar.SetActive(false);
+    cameraAnalisar.SetActive(false);
+    menuAnalisar.SetActive(false);
+    cameraEolica.SetActive(true);
+    painelAnaliseEolica.SetActive(true);
+}
+
+public void analisarOutro()
+{
+    cameraUIAnalisar.SetActive(true);
+    cameraAnalisar.SetActive(true);
+    menuAnalisar.SetActive(true);
+    cameraHidroeletrica.SetActive(false);
+    cameraEolica.SetActive(false);
+    cameraSolar.SetActive(false);
+    painelAnaliseHidroeletrica.SetActive(false);
+    painelAnaliseEolica.SetActive(false);
+    painelAnaliseSolar.SetActive(false);
 }
 
 public void iniciarExploração()
 {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
     menuInicial.SetActive(false);
     jogador.SetActive(true);
     cameraMenu.SetActive(false);
+}
+
+public void abrirAnalisar()
+{
+    menuInicial.SetActive(false);
+    menuAnalisar.SetActive(true);
+    cameraAnalisar.SetActive(true);
+    cameraMenu.SetActive(false);
+    cameraUIAnalisar.SetActive(true);
 }
 
 public void abrirOpcoes()
@@ -41,6 +126,9 @@ public void retornarAoMenu()
 {
     menuInicial.SetActive(true);
     menuOpcoes.SetActive(false);
+    cameraAnalisar.SetActive(false);
+    cameraUIAnalisar.SetActive(false);
+    menuAnalisar.SetActive(false);
     jogador.SetActive(false);
     cameraMenu.SetActive(true);
     cameraOpcoes.SetActive(false);
